b77af0baf204f62ae4c12a83bb227ea79d0580bc
anand <anandpromax@gmail.com>
2024-03-19 21:09:45 +0530

    Refactor SubmittedCode component table structure

--- src/components/SubmittedCode.jsx
+++ src/components/SubmittedCode.jsx
@@ -6,7 +6,7 @@ const entries = [
     stdin: "test",
     timestamp: 1234567890,
     sourceCode:
-      "print('hello world') rgjrj goeri jgoo rj lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "print('hello dolor sit amet, consectetur adipiscing elit.Nulla nec purus feugiat, vestibulum mi id, fermentum nunc.Integer sit amet ante vel arcu fermentum aliquam.') lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi id, fermentum nunc. Integer sit amet ante vel arcu fermentum aliquam." ,
   },
   {
     username: "test2",
@@ -19,32 +19,29 @@ const entries = [
 
 const SubmittedCode = () => {
   return (
-    <div className="flex flex-col w-full h-full">
-      <table className="p-2">
-        <thead className=" ">
-          <tr className="bg-gray-300">
-            <th>Username</th>
-            <th>Code Language</th>
-            <th>Stdin</th>
-            <th>Timestamp</th>
-            <th>Source Code</th>
+    <table className="table-fixed w-full text-sm text-left rtl:text-right text-gray-500 ">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50  ">
+        <tr className="bg-gray-300">
+          <th className="px-6 py-3">Username</th>
+          <th className="px-6 py-3">Code Language</th>
+          <th className="px-6 py-3">Stdin</th>
+          <th className="px-6 py-3">Timestamp</th>
+          <th className="px-6 py-3">Source Code</th>
+        </tr>
+      </thead>
+
+      <tbody className="divide-y divide-gray-200  ">
+        {entries.map((entry, index) => (
+          <tr key={index} className="hover:bg-gray-100 ">
+            <td className="px-6 py-4">{entry.username}</td>
+            <td className="px-6 py-4">{entry.codeLanguage}</td>
+            <td className="px-6 py-4">{entry.stdin}</td>
+            <td className="px-6 py-4">{new Date(entry.timestamp).toLocaleString()}</td>
+            <td className="px-6 py-4">{entry.sourceCode.slice(0, 100)}</td>
           </tr>
-        </thead>
-         
-          <tbody className="divide-y divide-gray-200 p-4">
-            {entries.map((entry, index) => (
-              <tr key={index} className="hover:bg-gray-100">
-                <td>{entry.username}</td>
-                <td>{entry.codeLanguage}</td>
-                <td>{entry.stdin}</td>
-                <td>{new Date(entry.timestamp).toLocaleString()}</td>
-                <td>{entry.sourceCode.slice(0, 100)}</td>
-              </tr>
-            ))}
-          </tbody>
-  
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
