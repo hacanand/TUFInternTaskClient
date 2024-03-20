41a0d319a4c646f35cddda57604a6fd624e22f5a
anand <anandpromax@gmail.com>
2024-03-21 00:52:44 +0530

    Refactor SubmittedCode component to fetch and display data from API

--- src/components/SubmittedCode.jsx
+++ src/components/SubmittedCode.jsx
@@ -1,36 +1,38 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-const entries = [
-  {
-    username: "test",
-    codeLanguage: "python",
-    stdin: "test",
-    timestamp: 1234567890,
-    sourceCode:
-      "print('hello dolor sit amet, consectetur adipiscing elit.Nulla nec purus feugiat, vestibulum mi id, fermentum nunc.Integer sit amet ante vel arcu fermentum aliquam.') lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi id, fermentum nunc. Integer sit amet ante vel arcu fermentum aliquam." ,
-  },
-  {
-    username: "test2",
-    codeLanguage: "python",
-    stdin: "test2",
-    timestamp: 1234567890,
-    sourceCode: "print('hello world')",
-  },
-  {
-    username: "test3",
-    codeLanguage: "python",
-    stdin: "test3",
-    timestamp: 1234567890,
-    sourceCode: "print('hello world')",
-  },
-];
+// const entries = [
+//   {
+//     username: "test",
+//     codeLanguage: "python",
+//     stdin: "test",
+//     timestamp: 1234567890,
+//     sourceCode:
+//       "print('hello dolor sit amet, consectetur adipiscing elit.Nulla nec purus feugiat, vestibulum mi id, fermentum nunc.Integer sit amet ante vel arcu fermentum aliquam.') lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, vestibulum mi id, fermentum nunc. Integer sit amet ante vel arcu fermentum aliquam." ,
+//   },
+//   {
+//     username: "test2",
+//     codeLanguage: "python",
+//     stdin: "test2",
+//     timestamp: 1234567890,
+//     sourceCode: "print('hello world')",
+//   },
+//   {
+//     username: "test3",
+//     codeLanguage: "python",
+//     stdin: "test3",
+//     timestamp: 1234567890,
+//     sourceCode: "print('hello world')",
+//   },
+// ];
 
 const SubmittedCode = () => {
+  const [entries, setEntries] = React.useState([]);
   useEffect(() => {
      async function fetchData() {
       const data = await axios.get("http://localhost:3000/api/users");
-      console.log(data.data);
+       setEntries(data.data);
+       console.log(data.data)
     }
     fetchData();
   }, []);
@@ -53,24 +55,32 @@ const SubmittedCode = () => {
         </thead>
 
         <tbody className="divide-y divide-gray-200  ">
-          {entries.map((entry, index) => (
-            <tr
-              key={index}
-              className="hover:bg-gray-100 text-gray-900 text-base "
-            >
-              <td className="px-6 py-4">{entry.username}</td>
-              <td className="px-6 py-4">{entry.codeLanguage}</td>
-              <td className="px-6 py-4">{entry.stdin}</td>
-              <td className="px-6 py-4">{entry.sourceCode.slice(0, 50)}</td>
-              <td className="px-6 py-4">
-                {new Date(entry.timestamp).toLocaleString()}
-              </td>
-              <td className="px-6 py-4">{entry.sourceCode.slice(0, 100)}</td>
-            </tr>
-          ))}
+          {entries &&
+            entries.map((entry, index) => (
+              <tr
+                key={index}
+                className="hover:bg-gray-100 text-gray-900 text-base "
+              >
+                <td className="px-6 py-4">{entry.user_name}</td>
+                <td className="px-6 py-4">{entry.code_language}</td>
+                <td className="px-6 py-4">{entry.stdin}</td>
+                <td className="px-6 py-4">{entry.stdout}</td>
+                <td className="px-6 py-4">
+                  {new Date(entry.createdAt).toLocaleString()}
+                </td>
+                <td className="px-6 py-4">
+                  {entry.source_code?.slice(0, 100)}
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
-      <button className=" mx-5 mt-10  p-2 bg-slate-400 text-white rounded-md" onClick={changePage}>Coding page</button>
+      <button
+        className=" mx-5 mt-10  p-2 bg-slate-400 text-white rounded-md"
+        onClick={changePage}
+      >
+        Coding page
+      </button>
     </>
   );
 };
